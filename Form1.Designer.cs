d1891dcb7d6af3dacfa52cdb443e053435f895d1
danik <baybolov_daniyar@mail.ru>
2022-02-22 01:11:47 +0600

    add security

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,8 +40,9 @@ namespace Serch_String
             this.btn_BM = new System.Windows.Forms.Button();
             this.btn_KMP = new System.Windows.Forms.Button();
             this.btn_straight = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label_Time = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -54,10 +55,10 @@ namespace Serch_String
             this.rtb_Basic.BackColor = System.Drawing.Color.RoyalBlue;
             this.rtb_Basic.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.rtb_Basic.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.rtb_Basic.Location = new System.Drawing.Point(21, 18);
-            this.rtb_Basic.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.rtb_Basic.Location = new System.Drawing.Point(13, 52);
+            this.rtb_Basic.Margin = new System.Windows.Forms.Padding(4);
             this.rtb_Basic.Name = "rtb_Basic";
-            this.rtb_Basic.Size = new System.Drawing.Size(1345, 242);
+            this.rtb_Basic.Size = new System.Drawing.Size(1009, 187);
             this.rtb_Basic.TabIndex = 0;
             this.rtb_Basic.Text = "";
             // 
@@ -68,10 +69,10 @@ namespace Serch_String
             this.rtb_sample.BackColor = System.Drawing.Color.RoyalBlue;
             this.rtb_sample.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.rtb_sample.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.rtb_sample.Location = new System.Drawing.Point(21, 359);
-            this.rtb_sample.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.rtb_sample.Location = new System.Drawing.Point(16, 292);
+            this.rtb_sample.Margin = new System.Windows.Forms.Padding(4);
             this.rtb_sample.Name = "rtb_sample";
-            this.rtb_sample.Size = new System.Drawing.Size(742, 96);
+            this.rtb_sample.Size = new System.Drawing.Size(748, 78);
             this.rtb_sample.TabIndex = 1;
             this.rtb_sample.Text = "";
             // 
@@ -86,9 +87,9 @@ namespace Serch_String
             this.btn_ClearTextSample.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btn_ClearTextSample.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.btn_ClearTextSample.Location = new System.Drawing.Point(0, 0);
-            this.btn_ClearTextSample.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btn_ClearTextSample.Margin = new System.Windows.Forms.Padding(4);
             this.btn_ClearTextSample.Name = "btn_ClearTextSample";
-            this.btn_ClearTextSample.Size = new System.Drawing.Size(113, 96);
+            this.btn_ClearTextSample.Size = new System.Drawing.Size(85, 78);
             this.btn_ClearTextSample.TabIndex = 3;
             this.btn_ClearTextSample.Text = "Очистить";
             this.btn_ClearTextSample.UseVisualStyleBackColor = false;
@@ -103,12 +104,12 @@ namespace Serch_String
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgv.BackgroundColor = System.Drawing.Color.RoyalBlue;
             this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv.Location = new System.Drawing.Point(333, 529);
-            this.dgv.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.dgv.Location = new System.Drawing.Point(250, 430);
+            this.dgv.Margin = new System.Windows.Forms.Padding(4);
             this.dgv.Name = "dgv";
             this.dgv.RowHeadersVisible = false;
             this.dgv.RowHeadersWidth = 51;
-            this.dgv.Size = new System.Drawing.Size(1033, 346);
+            this.dgv.Size = new System.Drawing.Size(775, 281);
             this.dgv.TabIndex = 7;
             // 
             // panel1
@@ -117,10 +118,9 @@ namespace Serch_String
             this.panel1.Controls.Add(this.btn_GenText);
             this.panel1.Controls.Add(this.btn_ClearTextBasic);
             this.panel1.Controls.Add(this.btn_ClearTextSample);
-            this.panel1.Location = new System.Drawing.Point(1029, 359);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Location = new System.Drawing.Point(772, 292);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(337, 96);
+            this.panel1.Size = new System.Drawing.Size(253, 78);
             this.panel1.TabIndex = 8;
             // 
             // btn_GenText
@@ -133,13 +133,14 @@ namespace Serch_String
             this.btn_GenText.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_GenText.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btn_GenText.ForeColor = System.Drawing.Color.Lime;
-            this.btn_GenText.Location = new System.Drawing.Point(224, 0);
-            this.btn_GenText.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btn_GenText.Location = new System.Drawing.Point(168, 0);
+            this.btn_GenText.Margin = new System.Windows.Forms.Padding(4);
             this.btn_GenText.Name = "btn_GenText";
-            this.btn_GenText.Size = new System.Drawing.Size(113, 96);
+            this.btn_GenText.Size = new System.Drawing.Size(85, 78);
             this.btn_GenText.TabIndex = 5;
             this.btn_GenText.Text = "Генерация текста";
             this.btn_GenText.UseVisualStyleBackColor = false;
+            this.btn_GenText.Click += new System.EventHandler(this.btn_GenText_Click);
             // 
             // btn_ClearTextBasic
             // 
@@ -151,10 +152,10 @@ namespace Serch_String
             this.btn_ClearTextBasic.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_ClearTextBasic.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btn_ClearTextBasic.ForeColor = System.Drawing.Color.MintCream;
-            this.btn_ClearTextBasic.Location = new System.Drawing.Point(112, 0);
-            this.btn_ClearTextBasic.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btn_ClearTextBasic.Location = new System.Drawing.Point(84, 0);
+            this.btn_ClearTextBasic.Margin = new System.Windows.Forms.Padding(4);
             this.btn_ClearTextBasic.Name = "btn_ClearTextBasic";
-            this.btn_ClearTextBasic.Size = new System.Drawing.Size(113, 96);
+            this.btn_ClearTextBasic.Size = new System.Drawing.Size(85, 78);
             this.btn_ClearTextBasic.TabIndex = 4;
             this.btn_ClearTextBasic.Text = "Очистить";
             this.btn_ClearTextBasic.UseVisualStyleBackColor = false;
@@ -165,10 +166,9 @@ namespace Serch_String
             this.panel2.Controls.Add(this.btn_BM);
             this.panel2.Controls.Add(this.btn_KMP);
             this.panel2.Controls.Add(this.btn_straight);
-            this.panel2.Location = new System.Drawing.Point(21, 497);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Location = new System.Drawing.Point(16, 404);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(303, 361);
+            this.panel2.Size = new System.Drawing.Size(227, 293);
             this.panel2.TabIndex = 9;
             // 
             // btn_BM
@@ -181,10 +181,10 @@ namespace Serch_String
             this.btn_BM.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_BM.Font = new System.Drawing.Font("Sitka Small", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_BM.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.btn_BM.Location = new System.Drawing.Point(-4, 260);
-            this.btn_BM.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btn_BM.Location = new System.Drawing.Point(-3, 211);
+            this.btn_BM.Margin = new System.Windows.Forms.Padding(4);
             this.btn_BM.Name = "btn_BM";
-            this.btn_BM.Size = new System.Drawing.Size(279, 96);
+            this.btn_BM.Size = new System.Drawing.Size(209, 78);
             this.btn_BM.TabIndex = 10;
             this.btn_BM.Text = "Боуера и Мура.";
             this.btn_BM.UseVisualStyleBackColor = false;
@@ -199,10 +199,10 @@ namespace Serch_String
             this.btn_KMP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_KMP.Font = new System.Drawing.Font("Sitka Small", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_KMP.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.btn_KMP.Location = new System.Drawing.Point(0, 129);
-            this.btn_KMP.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btn_KMP.Location = new System.Drawing.Point(0, 105);
+            this.btn_KMP.Margin = new System.Windows.Forms.Padding(4);
             this.btn_KMP.Name = "btn_KMP";
-            this.btn_KMP.Size = new System.Drawing.Size(275, 96);
+            this.btn_KMP.Size = new System.Drawing.Size(206, 78);
             this.btn_KMP.TabIndex = 9;
             this.btn_KMP.Text = "Кнута, Морриса и Пратта";
             this.btn_KMP.UseVisualStyleBackColor = false;
@@ -218,26 +218,14 @@ namespace Serch_String
             this.btn_straight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_straight.Font = new System.Drawing.Font("Sitka Small", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_straight.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.btn_straight.Location = new System.Drawing.Point(0, 5);
-            this.btn_straight.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btn_straight.Location = new System.Drawing.Point(0, 4);
+            this.btn_straight.Margin = new System.Windows.Forms.Padding(4);
             this.btn_straight.Name = "btn_straight";
-            this.btn_straight.Size = new System.Drawing.Size(275, 96);
+            this.btn_straight.Size = new System.Drawing.Size(206, 78);
             this.btn_straight.TabIndex = 8;
             this.btn_straight.Text = "Прямой метод";
             this.btn_straight.UseVisualStyleBackColor = false;
-            this.btn_straight.Click += new System.EventHandler(this.button3_Click_1);
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.BackColor = System.Drawing.Color.RoyalBlue;
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(756, 359);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(264, 96);
-            this.label1.TabIndex = 10;
+            this.btn_straight.Click += new System.EventHandler(this.btn_straight_Click);
             // 
             // label_Time
             // 
@@ -245,27 +233,51 @@ namespace Serch_String
             this.label_Time.BackColor = System.Drawing.Color.Transparent;
             this.label_Time.Font = new System.Drawing.Font("Sitka Small", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label_Time.ForeColor = System.Drawing.Color.MintCream;
-            this.label_Time.Location = new System.Drawing.Point(326, 482);
-            this.label_Time.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_Time.Location = new System.Drawing.Point(244, 392);
             this.label_Time.Name = "label_Time";
-            this.label_Time.Size = new System.Drawing.Size(128, 42);
+            this.label_Time.Size = new System.Drawing.Size(102, 33);
             this.label_Time.TabIndex = 12;
             this.label_Time.Text = "Время:";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.MintCream;
+            this.label1.Location = new System.Drawing.Point(7, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 33);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Введите слово:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Sitka Small", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.MintCream;
+            this.label2.Location = new System.Drawing.Point(7, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 33);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Введите текст:";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Serch_String.Properties.Resources._1616610153_47_p_sinii_fon_kosmos_53;
-            this.ClientSize = new System.Drawing.Size(1398, 884);
-            this.Controls.Add(this.label_Time);
+            this.ClientSize = new System.Drawing.Size(1048, 718);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_Time);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dgv);
             this.Controls.Add(this.rtb_sample);
             this.Controls.Add(this.rtb_Basic);
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
@@ -289,8 +301,9 @@ namespace Serch_String
         private System.Windows.Forms.Button btn_BM;
         private System.Windows.Forms.Button btn_KMP;
         private System.Windows.Forms.Button btn_straight;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label_Time;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -14,64 +15,62 @@ namespace Serch_String
       
         }
      
-        private void button1_Click_1(object sender, EventArgs e)
-        {
 
-        }
 
-        private void button3_Click_1(object sender, EventArgs e)
+        private void btn_straight_Click(object sender, EventArgs e)
         {
             String with = Convert.ToString(rtb_Basic.Text);
             String what = Convert.ToString(rtb_sample.Text);
             int count = 0;
             char[] n = with.ToCharArray();
             char[] m = what.ToCharArray();
+            bool check = false;
 
-
-            for (int i = 0; i <= n.Length - m.Length; i++)
+            if (rtb_Basic.Text != "" && rtb_sample.Text !="") 
             {
-                for (int j = 0; j < m.Length; j++)
-                {
-                    if (n[i + j] == m[j])
-                    {
-                        count++;
-                        if (count == m.Length)
-                        {
-                            MessageBox.Show("Nice");
-                            rtb_Basic.SelectionStart = rtb_Basic.TextLength;
-                            rtb_Basic.SelectionLength = 0;
-                            rtb_Basic.SelectionColor = Color.Red;
-                            rtb_Basic.AppendText(Environment.NewLine + what);
-                            rtb_Basic.SelectionColor = rtb_Basic.ForeColor;
+                Stopwatch time1 = new Stopwatch();
 
-                            //string mystring = @"my first string";
-                            //if (richTextBox1.Find(n) > 0)
-                            //{
-                            //    int my1stPosition = richTextBox1.Find(what);
-                            //    richTextBox1.SelectionStart = my1stPosition;
-                            //    richTextBox1.SelectionLength = with.Length;
-                            //    richTextBox1.SelectionColor = Color.Red;
-                            //}
+                time1.Start();
+                for (int i = 0; i <= n.Length - m.Length; i++)
+                {
+
+                    for (int j = 0; j < m.Length; j++)
+                    {
+                        if (n[i + j] == m[j])
+                        {
+                            count++;
+                            if (count == m.Length)
+                            {
+                                check = true;
+                                MessageBox.Show("Nice");
+                                rtb_Basic.SelectionStart = rtb_Basic.TextLength;
+                                rtb_Basic.SelectionLength = 0;
+                                rtb_Basic.SelectionColor = Color.Red;
+                                rtb_Basic.AppendText(Environment.NewLine + what);
+                                rtb_Basic.SelectionColor = rtb_Basic.ForeColor;                        
+                            }
+                        }
+                        else
+                        {
+                            count = 0;
                         }
                     }
-                    else
-                    {
-                        count = 0;
-                    }
                 }
+                time1.Stop();
+                label_Time.Text = time1.ElapsedTicks.ToString();
+                
+
+                if (!check) MessageBox.Show("Текст не найден");
             }
-
-
-        }
-
-
-        
+            else MessageBox.Show("Введите текст");
+        }        
 
         private void prefix_function(string sample, int[] samplePrefis)
         {
             samplePrefis[0] = 0;
             int j = 0;
             int i = 1;
+
             while (i < sample.Length)
             {
                 if (sample[i] != sample[j])
@@ -92,56 +91,70 @@ namespace Serch_String
                     j++;
                 }
             }
-        }
-        private void genText()
-        {
-            const string letterSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSRUVWXYZ      ";
 
         }
+  
 
         private void btn_KMP_Click(object sender, EventArgs e)
         {
+
             dgv.Visible = false;
             dgv.Rows.Clear();
             dgv.Columns.Clear();
             string sample = rtb_sample.Text;
             string basic = rtb_Basic.Text;
-
+            if (rtb_Basic.Text != "" && rtb_sample.Text != "")
+            {
             int[] samplePrefis = new int[sample.Length];
             prefix_function(sample, samplePrefis);
-            for(int a = 0; a<basic.Length;a++)
-            {
-                dgv.Columns.Add("DGV1", Convert.ToString(basic[a]));
-                dgv.Columns[a].Width = dgv.Width / 20;
-            }
-            int i = 0;
-            int j = 0;
-            while (true)
-            {
-                if (basic[i] == sample[j])
+          
+                for (int a = 0; a < basic.Length; a++)
                 {
-                    i++; j++;
-                    if (j == sample.Length - 1)
+                    dgv.Columns.Add("DGV1", Convert.ToString(basic[a]));
+                    dgv.Columns[a].Width = dgv.Width / 20;
+                }
+                int i = 0;
+                int j = 0;
+                while (true)
+                {
+                    if (basic[i] == sample[j])
                     {
-                        MessageBox.Show("Well done!!!");
-                        break;
+                        i++; j++;
+                        if (j == sample.Length - 1)
+                        {
+                            MessageBox.Show("Well done!!!");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (j > 0)
+                        {
+                            j = samplePrefis[j - 1];
+                        }
+                        else i++;
+                        if (i == basic.Length)
+                        {
+                            MessageBox.Show("No found!!!");
+                            break;
+                        }
                     }
                 }
-                else
-                {
-                    if (j > 0)
-                    {
-                        j = samplePrefis[j - 1];
-                    }
-                    else i++;
-                    if (i == basic.Length)
-                    {
-                        MessageBox.Show("No found!!!");
-                        break;
-                    }
-                }
+                dgv.Visible = false;
             }
-            dgv.Visible = false;
+            else MessageBox.Show("Введите текст");
+        }
+
+        private void btn_GenText_Click(object sender, EventArgs e)
+        {
+            string s = "", symb = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSRUVWXYZ             ";
+            Random rnd = new Random();
+
+            for (int i = 0; i < symb.Length; i++)
+            {
+                s += symb[rnd.Next(0, symb.Length)];
+                rtb_Basic.Text = s;
+            }
         }
     }
 }
