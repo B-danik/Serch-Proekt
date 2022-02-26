cca70ebe1330b368b17d26875a33ffad3bc24de5
danik <baybolov_daniyar@mail.ru>
2022-02-26 22:55:03 +0600

    commit

--- Form1.cs
+++ Form1.cs
@@ -1,79 +1,78 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Drawing;
 using System.Windows.Forms;
 
 namespace Serch_String
 {
     public partial class Form1 : Form
     {    
-
         public Form1()
         {
             InitializeComponent();
-
         }
 
         private void btn_GenText_Click(object sender, EventArgs e)
         {
             string s = "", symb = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSRUVWXYZ             ";
             Random rnd = new Random();
-
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 20; i++)
             {
                 s += symb[rnd.Next(0, symb.Length - 1)];
-                rtb_Basic.Text = s;
-                {
-                if (rnd.Next(0, symb.Length - 1) == 0)
-                    s += "ANNA";
-                 }
+                rtb_Basic.Text = s;               
             }
         }
 
 
         private void btn_straight_Click(object sender, EventArgs e)
         {
-            String with = Convert.ToString(rtb_Basic.Text);
-            String what = Convert.ToString(rtb_sample.Text);
-            int count = 0;
-            char[] n = with.ToCharArray();
-            char[] m = what.ToCharArray();
-            bool check = false;
-
+            int numRow = 0;
+            dgv.Visible = false;
+            dgv.Rows.Clear();
+            dgv.Columns.Clear();
+            string sample = rtb_sample.Text; 
+            string basic = rtb_Basic.Text;           
+            int count = 0;     
+            bool check = false;                
             if (rtb_Basic.Text != "" && rtb_sample.Text != "")
-            {
+            {               
+                for (int a = 0; a < basic.Length; a++)
+                {
+                    dgv.Columns.Add("DGV1", Convert.ToString(basic[a]));
+                    dgv.Columns[a].Width = dgv.Width / 20;
+                }         
                 Stopwatch time1 = new Stopwatch();
                 time1.Start();
-                for (int i = 0; i <= n.Length - m.Length; i++)
+                for (int i = 0; i <= basic.Length - sample.Length; i++)                    
                 {
-
-                    for (int j = 0; j < m.Length; j++)
+                    dgv.Rows.Add();
+                    for (int b = i, k = 0; k < sample.Length; k++,b++)
                     {
-                        if (n[i + j] == m[j])
-                        {
+                        dgv[b, numRow].Value = sample[k];
+                    }
+                    for (int j = 0; j < sample.Length; j++)
+                    {
+                       
+                        if (basic[i + j] == sample[j])
+                        {                     
                             count++;
-                            if (count == m.Length)
+                            if (count == sample.Length)
                             {
                                 check = true;
-                                MessageBox.Show("Nice");
-                                rtb_Basic.SelectionStart = rtb_Basic.TextLength;
-                                rtb_Basic.SelectionLength = 0;
-                                rtb_Basic.SelectionColor = Color.Red;
-                                rtb_Basic.AppendText(Environment.NewLine + what);
-                                rtb_Basic.SelectionColor = rtb_Basic.ForeColor;
+                                MessageBox.Show("Well done");                               
                             }
                         }
                         else
                         {
                             count = 0;
-                        }
+                        }                                  
                     }
+                    numRow++;    
                 }
                 time1.Stop();
                 label_Time.Text = time1.ElapsedTicks.ToString();
 
-
                 if (!check) MessageBox.Show("Текст не найден");
+                dgv.Visible = true;
             }
             else MessageBox.Show("Введите текст");
         }
@@ -83,7 +82,6 @@ namespace Serch_String
             samplePrefis[0] = 0;
             int j = 0;
             int i = 1;
-
             while (i < sample.Length)
             {
                 if (sample[i] != sample[j])
@@ -156,9 +154,7 @@ namespace Serch_String
                         }
                         else
                         {
-
                             i++;
-
                         }
                         if (i == basic.Length)
                         {
@@ -171,14 +167,14 @@ namespace Serch_String
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btn_ClearTextSample_Click(object sender, EventArgs e)
         {
-
+            rtb_Basic.Clear();
         }
 
-        private void btn_GenText_Click_1(object sender, EventArgs e)
+        private void btn_ClearTextBasic_Click(object sender, EventArgs e)
         {
-
+            rtb_sample.Clear();
         }
     }
 }
